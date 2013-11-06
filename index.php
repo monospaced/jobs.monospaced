a418b11ece9de0dfb20926254e42e78cf359a78c
Scott Boyle <scott@monospaced.com>
2013-11-06 10:49:55 +0000

    relative url for json

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	date_default_timezone_set('Europe/London');
-	$url ='http://jobs.monospaced.com/cache/rss.json';
+	$url ='cache/rss.json';
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
